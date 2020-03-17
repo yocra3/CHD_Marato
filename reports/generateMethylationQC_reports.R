b73c4fd8b0b6b8d8afb318d112acf5b0f411621e
Carlos Ruiz <yocra3@gmail.com>
2020-03-17 09:41:20 +0100

    Repeat methylation analysis with updated phenotypes

--- reports/generateMethylationQC_reports.R
+++ reports/generateMethylationQC_reports.R
@@ -9,7 +9,7 @@
 library(meffil)
 
 ## Define names
-vers <- "v1"
+vers <- "v2"
 QC_folder <- paste0("results/methylation/QC_intermediate/", vers)
 reportsName <- paste("reports/meffil", vers, Sys.Date(), sep = "_")
 
@@ -22,16 +22,3 @@ meffil.qc.report(qc.summary, output.file = paste0(reportsName, "_clean.html"))
 
 load(paste0(QC_folder, "/norm.summary.Rdata"))
 meffil.normalization.report(norm.summary, output.file = paste0(reportsName, "_normalization.html"))
-
-
-
-# ## Check correlation in duplicated sample
-# cpgMeans <- rowMeans(norm.beta)
-# correlsglobal <- cor(norm.beta - cpgMeans)
-# 
-# ## Add to report
-# hist(correlsglobal[upper.tri(correlsglobal)], main = "Mean-centered correlation", xlab = "Pearson r")
-# median(correlsglobal[upper.tri(correlsglobal)])
-# correlsglobal["G2CS12COBIS", "G2CS12CO"]
-# ## Median centered correlation is close to 0, as expected
-# ## Highest correlation is found for duplicated samples (G2CS12CO)
